358b3573dafa8b873d577e87cf3b048e89f83076
Quy Duc Do <ducquy2200@gmail.com>
2026-02-19 13:51:38 -0500

    update file name and unused param

--- frontend/src/store/useAppStore.js
+++ frontend/src/store/useAppStore.js
@@ -3,7 +3,7 @@ import { persist } from 'zustand/middleware'
 
 export const useAppStore = create(
   persist(
-    (set, get) => ({
+    (set) => ({
   // Document
   documentId: null,
   documentStatus: 'idle',
